1c045dd01c1babe1609ba2ef11c36a7a5f19b61a
MarceloMMendes <marcelo.mendes@maxmilhas.com.br>
2025-03-13 22:11:55 -0300

    first commit

---
+++ src/main/java/com/example/account/main/AccountMainServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.account.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountMainServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountMainServiceApplication.class, args);
+	}
+
+}
